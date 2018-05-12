ef9b8d3a9e476777b8fed9972c8cdc20d2e3636b
Alexys Gonzalez <alexysbike@gmail.com>
2018-05-12 15:46:20 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "root": true,
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest": true,
+    "node": true
+  }
+};
