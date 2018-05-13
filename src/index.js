31f74b5b5f0aed5b9ba2ae13e9e2c1a7a2568d19
Alexys Gonzalez <alexysbike@gmail.com>
2018-05-12 21:17:21 -0400

    Installing bulma and proptypes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.min.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
