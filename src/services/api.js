46140ccd409996dc4ce4e055aac4e0fe0d67e7e3
Alexys Gonzalez <alexysbike@gmail.com>
2018-05-13 01:27:33 -0400

    Connecting to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,10 @@
-const mockedData = [
-  {
-    contractUntil: '2022-06-30',
-    dateOfBirth: '1993-05-13',
-    jerseyNumber: 9,
-    name: 'Romelu Lukaku',
-    nationality: 'Belgium',
-    position: 'Centre-Forward',
-  }, {
-    contractUntil: '2019-06-30',
-    dateOfBirth: '1990-11-07',
-    jerseyNumber: 1,
-    name: 'David de Gea',
-    nationality: 'Spain',
-    position: 'Keeper',
-  }, {
-    contractUntil: '2021-06-30',
-    dateOfBirth: '1987-02-22',
-    jerseyNumber: 20,
-    name: 'Sergio Romero',
-    nationality: 'Argentina',
-    position: 'Keeper',
-  },
-];
+import axios from 'axios';
+import { API_PATH } from '../config/paths';
 
 class api {
-  static getAll() {
-    return new Promise(resolve => resolve(mockedData));
+  static async getAll() {
+    const response = await axios.get(API_PATH);
+    return response.data;
   }
 }
 
